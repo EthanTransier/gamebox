ba2aa5e00c05dca7522e8947b38ef06390566715
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2023-10-18 13:20:34 -0700

    Fixed pathing issue & Wireframe

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // uses the public foler
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('./public'));
 // fixes MIME type issues
 
 // Express session
